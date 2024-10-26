9bd4165f3eeeeb40ebc135299981739d2b666ef0
Irshad Nizami <irniz@rediffmail.com>
2024-10-26 22:29:46 +0530

    Modify Jenkinsfile and groovy script to use library

--- script.groovy
+++ script.groovy
@@ -1,19 +1,3 @@
-#!/user/bin/env groovy
-@Library('jenkins-shared-library')
-def gv
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t irschad/java-app:2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push irschad/java-app:2.0'
-    }
-}
 
 def deployApp() {
     echo 'deploying the application...'
